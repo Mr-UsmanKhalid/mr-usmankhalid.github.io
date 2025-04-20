f430860a414910cc7cf545ddfae7367e4fa69766
UsmanQ132 <usmanq0139@gmail.com>
2025-04-20 16:14:24 +0500

    Update Project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,7 @@ import "./Styles/projects.css";
 import projectimage1 from "../Assets/Aptech_Appliaction_Screens.png";
 import projectimage2 from "../Assets/Car_Website.png";
 import projectimage3 from "../Assets/MyPortfolio.png";
+import projectimage4 from "../Assets/quote.png";
 
 function Project() {
   const projects = [
@@ -21,11 +22,18 @@ function Project() {
       Link: "https://github.com/Mr-UsmanKhalid/Car-front-end-Web-Design"
     },
     {
-      id: 2,
+      id: 3,
       image: projectimage3,
       title: "Personal Portfolio",
       description: "I create modern, responsive, and high-performance applications using React. Passionate about delivering seamless user experiences with clean UI/UX and efficient functionality.",
       Link: "https://github.com/Mr-UsmanKhalid/Responsive-Portfolio-With-React"
+    },
+    {
+      id: 4,
+      image: projectimage4,
+      title: "Random Quote App",
+      description: "Built a Random Quote Generator using API integration,Fetched quotes via GET requests,React-based UI with dynamic rendering.",
+      Link: "https://github.com/Mr-UsmanKhalid/Random-Quote-App"
     },
   ];
   return (

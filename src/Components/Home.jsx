088b02fdae19b94bdc4ec1aa2ab12e8f70336c3a
Mr-UsmanKhalid <usmanq0139@gmail.com>
2026-08-05 10:45:09 +0500

    Update Resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
       <h2>{text} |</h2>
 
         <div className="btn-sci">
-        <a href="https://drive.google.com/file/d/1cC1Y1-f84YV2rCFwO-3VBGodiL5FK-rE/view?usp=drive_link" target="blank" className="btn">Download Resume</a>
+        <a href="https://drive.google.com/file/d/1Wn4uZpydb9MP31zHwugwqG1UduWawfz5/view?usp=sharing" target="blank" className="btn">Download Resume</a>
           <div className="sci">
             <a href="https://github.com/Mr-UsmanKhalid" target="_blank"><i class="bx bxl-github"></i></a>&nbsp;&nbsp;&nbsp;
             <a href="https://www.linkedin.com/in/usman-khalid-21a656343/" target="_blank"><i class="bx bxl-linkedin"></i></a>&nbsp;&nbsp;&nbsp;
